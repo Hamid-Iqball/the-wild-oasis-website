72b34c2ed481ccbf69250bc8b77a0a003db99a69
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2025-02-05 23:41:30 +0500

    Data fetching in Next.js

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,19 @@
 
 
-function page() {
+async function page() {
+
+const res = await fetch("https://jsonplaceholder.typicode.com/users")
+const data = await res.json()
+console.log(data)
   return (
     <div>
-<h1>Cabins</h1>
+   <h1>Cabins</h1>
 
+<ul>
+  {data.map((user)=>(
+    <li key={user.id}>{user.name}</li>
+  ))}
+</ul>
     </div>
   )
 }
