ca77468062e8c970f64527a1116016091eb0c225
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-02-11 23:59:43 +0500

    sideNavigation

---
+++ app/account/layout.js
@@ -0,0 +1,8 @@
+import SideNavigation from "../_components/SideNavigation";
+
+export default function Layout({children}){
+    return <div className="grid grid-cols-[16rem,1fr] gap-12">
+        <SideNavigation/>
+        <div className="py-1">{children}</div>
+    </div>
+}
