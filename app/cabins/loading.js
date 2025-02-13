e2ef3c09b075c8ee62bd1591682e27f11839dab8
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-02-13 17:57:20 +0500

    spinner

--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -3,9 +3,9 @@ import Spinner from "@/app/_components/Spinner"
 
 function loading() {
   return (
-    <div className="grid items-center justify-center">
-<Spinner/>
-<p className="text-xl text-primary-200">Loading Cabin data ...</p>
+    <div className="grid items-center justify-center text-center">
+    <Spinner/>
+    <p className="text-xl text-primary-200">Loading Cabin data ...</p>
     </div>
   )
 }
