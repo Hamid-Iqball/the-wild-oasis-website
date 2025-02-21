5ec43e3f2a34d08d282c6612d73fb950850f3f7e
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-02-21 22:45:02 +0500

    Get user session  through auth

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  console.log("heu")
+  const session = await auth()
+  console.log(session)
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
