3a9251ffecd5d46962ecf37d2bf94d06ba40e722
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-02-24 16:01:45 +0500

    useFornmstatus hook to know about the server action

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,19 +1,19 @@
 "use client"
 import { useState } from "react"
 import { updateProfile } from "../_lib/actions"
+import { useFormStatus } from "react-dom"
 
 
 
 function UpdateProfileForm({guest,children}) {
-    const [count, setCount] = useState()
+   
     const {fullName, email,nationalID, countryFlag, nationality}  = guest
- 
-    //There are two types of validation, one is time validation after certian time the cache will revalidate and the data will chnage, second one is the manual/on-demand validation where we validate the cache immediatly
     
-  return (
-    //Invoking the server action
-    <form action={updateProfile}
-    className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    //This is the new hook in react , and this hook must be used in a component that rendered inside a form, not a components that simply contains a form.
+    return (
+      //Invoking the server action
+      <form action={updateProfile}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
     <div className="space-y-2">
       <label>Full name</label>
       <input
@@ -21,7 +21,7 @@ function UpdateProfileForm({guest,children}) {
         defaultValue={fullName}
         name="fullName"
         className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-      />
+        />
     </div>
 
     <div className="space-y-2">
@@ -31,7 +31,7 @@ function UpdateProfileForm({guest,children}) {
         defaultValue={email}
         name="email"
         className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-      />
+        />
     </div>
 
     <div className="space-y-2">
@@ -41,7 +41,7 @@ function UpdateProfileForm({guest,children}) {
           src={countryFlag}
           alt="Country flag"
           className="h-5 rounded-sm"
-        />
+          />
       </div>
 
         {children}
@@ -54,16 +54,23 @@ function UpdateProfileForm({guest,children}) {
       defaultValue={nationalID}
         name="nationalID"
         className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-      />
+        />
     </div>
 
     <div className="flex justify-end items-center gap-6">
-      <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-        Update profile
-      </button>
+    <Button/>
     </div>
   </form>
   )
+}
+
+//We will use this component inside the form where we have used the useFormStatus()
+function Button (){
+   const {pending} = useFormStatus()
+  return   <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+  disabled={pending}>
+ {pending ? "Updating":"Update profile"}
+</button>
 }
 
 export default UpdateProfileForm
